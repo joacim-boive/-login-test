33403cec9aa341dea580a9ab43c58670ed63cc47
Johan Linderoth <joli44@ecster.se>
2018-05-18 19:18:24 +0100

    manual cherry picking from cordova branch

---
+++ cordova/www/cordova/index.js
@@ -0,0 +1,25 @@
+window.cordovaApp = true;
+// window.GAConf = {basePath: 'ecster', id: 'UA-72858010-2', v: '1487870554077', env: 'dev'};
+
+window.ecsterAppConfig = {
+    // TODO: How..? Webpack?
+    ajaxBaseUrl: 'https://secure.ecster.se',
+    // basePath: 'ecster',
+};
+
+// cordova generated + modified
+const app = {
+    initialize() {
+        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+    },
+
+    onDeviceReady() {
+        console.log('Received deviceready event');
+        // initApplication defined in src/index.js
+        if (window.initApplication) {
+            window.initApplication(window.ecsterAppConfig);
+        }
+    },
+};
+
+app.initialize();
