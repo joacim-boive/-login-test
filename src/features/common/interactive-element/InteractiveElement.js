ceff8b5c043413d95c4f49555c299dbbc775ba5c
David Tatti <data05@handelsbanken.se>
2018-05-31 13:52:08 +0100

    added InteractiveElement and changed mobile sub menu to use it instead of interactive div

---
+++ src/features/common/interactive-element/InteractiveElement.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'classnames';
+import './InteractiveElement.scss';
+
+export const InteractiveElement = ({ className, children, ...rest }) => {
+    const classes = classNames({
+        'interactive-element': true,
+        [className]: className,
+    });
+    return (
+        <button className={classes} {...rest}>
+            {children}
+        </button>
+    );
+};
+
+InteractiveElement.propTypes = {
+    className: PropTypes.string,
+    children: PropTypes.node,
+};
+
+InteractiveElement.defaultProps = {
+    className: '',
+    children: <div />,
+}
