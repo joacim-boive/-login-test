2e5d5016dc99c61f25227fddb170b6b22aabed87
David Tatti <data05@handelsbanken.se>
2018-06-08 15:54:37 +0100

    added user mock data and stopped jest from running /tests/

--- tests/features/customer/AccountTransactionsOverview.test.js
+++ tests/features/customer/AccountTransactionsOverview.test.js
@@ -1,19 +1,18 @@
 import React from 'react';
-import { shallow } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import Enzyme, { shallow } from 'enzyme';
 import { AccountTransactionsOverview } from '../../../src/features/customer/AccountTransactionsOverview';
 
-describe('customer/AccountTransactionsOverview', () => {
-  it('renders node with correct class name', () => {
-    const props = {
-      account: {},
-      actions: {},
-    };
-    const renderedComponent = shallow(
-      <AccountTransactionsOverview {...props} />
-    );
+Enzyme.configure({ adapter: new Adapter() });
 
-    expect(
-      renderedComponent.find('.customer-account-transactions-overview').length
-    ).toBe(1);
-  });
+describe('customer/AccountTransactionsOverview', () => {
+    it('renders node with correct class name', () => {
+        const props = {
+            account: {},
+            actions: {},
+        };
+        const renderedComponent = shallow(<AccountTransactionsOverview {...props} />);
+
+        expect(renderedComponent.find('.customer-account-transactions-overview').length).toBe(1);
+    });
 });
