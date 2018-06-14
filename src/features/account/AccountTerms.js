cba56ca1f9ef9d845e2f5ada3b66007d6ab9b194
Johan Linderoth <johan@jlkonsultab.se>
2018-06-14 08:25:16 +0200

    w.i.p.

--- src/features/account/AccountTerms.js
+++ src/features/account/AccountTerms.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 // import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
+import { getText as i18n } from "@ecster/ecster-i18n";
+
 import AuthenticatedSubPageTemplate from '../common/templates/AuthenticatedSubPageTemplate';
 import WhiteBox from '../common/white-box';
 // import * as actions from './redux/actions';
 import { getAccountTerms } from './redux/actions';
+
+const InfoLine = ({ label, value, description }) => (
+    <div className="info-line">
+        <div className="item label">{label}</div>
+        <div className="item value">{value}</div>
+        <div className="item description">{description}</div>
+    </div>
+);
+
+InfoLine.propTypes = {
+    label: PropTypes.string.isRequired,
+    value: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+};
 
 export class AccountTerms extends Component {
     componentWillMount() {}
@@ -15,7 +31,7 @@ export class AccountTerms extends Component {
             <AuthenticatedSubPageTemplate header="Kontovillkor">
                 <h1>Villkor</h1>
                 <WhiteBox className="account-account-terms">
-                    <div>Page Content: account/AccountTerms</div>
+                    <InfoLine label={} value="" description="" />
                 </WhiteBox>
             </AuthenticatedSubPageTemplate>
         );
