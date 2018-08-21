98b40a86d86d0cee949b77fcb0ba32d8ca62eaa9
Johan Linderoth <joli44@ecster.se>
2018-08-21 10:15:40 +0100

    removed commented code, i18n'd a loan component

--- src/features/loan/components/SETermsApply.js
+++ src/features/loan/components/SETermsApply.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
+import { getText as i18n } from '@ecster/ecster-i18n/lib/Translate';
 import { OrderedList } from '@ecster/ecster-components';
 import './SETerms.scss';
 
@@ -11,20 +12,22 @@ export const SETermsApply = ({ className }) => {
     });
     return (
         <div className={classes}>
-            <h4>Så här ansöker du om lånet</h4>
-            {/*<p>*/}
-                {/*<strong className="e-error">Inga texter är språkhanterade!</strong>*/}
-            {/*</p>*/}
+            <h4>{i18n('loan.terms-apply.header')}</h4>
             <OrderedList>
-                <span>Fyll i ansökan</span>
-                <span>Vid godkänd ansökan, signera skuldebrevet och skicka till oss</span>
-                <span>När vi fått skuldebrevet har du normalt sett pengarna på ditt konto inom ca tre bankdagar</span>
+                {i18n('loan.terms-apply.how-to-list', {
+                    returnObjects: true,
+                    wrapper: { tag: 'span' },
+                })}
             </OrderedList>
 
             <section>
-                <h5>För att kunna ansöka måste du uppfylla följande</h5>
-                <p>Du har en stadigvarande inkomst. Du får inte ha några betalningsanmärkningar.</p>
-                <p>Har du några frågor så kontakta oss på 08-33 01 30</p>
+                <h5>{i18n('loan.terms-apply.sub-header')}</h5>
+                <p>
+                    {i18n('loan.terms-apply.terms-list', {
+                        returnObjects: true,
+                        wrapper: { tag: 'p', dangerouslySetInnerHTML: true },
+                    })}
+                </p>
             </section>
         </div>
     );
