c36c0accba17a9af7e41d07ddf146d3b36a2077a
Johan Linderoth <joli44@ecster.se>
2018-09-27 11:49:02 +0100

    Show terminated acccounts

--- src/features/account/OverviewPage.js
+++ src/features/account/OverviewPage.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import { getText as i18n } from '@ecster/ecster-i18n/lib/Translate';
+import { Panel, ResponsivePanel } from '@ecster/ecster-components';
 import AuthenticatedPageTemplate from '../common/templates/AuthenticatedPageTemplate';
 import FeedbackPanel from '../home/FeedbackPanel';
 import { getAccounts } from './redux/getAccounts';
 import AccountPanel from './components/AccountPanel';
+import AccountPanelTerminatedAccount from './components/AccountPanelTerminatedAccount';
 import NoAccountsPanel from './no-account/NoAccountsPanel';
 
 export class OverviewPage extends Component {
     static propTypes = {
         accountsActive: PropTypes.array.isRequired,
+        accountsTerminated: PropTypes.array,
         user: PropTypes.object.isRequired,
         getAccounts: PropTypes.func.isRequired,
         hasZeroAccounts: PropTypes.bool.isRequired,
+    };
+
+    static defaultProps = {
+        accountsTerminated: [],
     };
 
     componentWillMount() {
@@ -31,16 +38,35 @@ export class OverviewPage extends Component {
     }
 
     render() {
-        const { accountsActive, user, hasZeroAccounts } = this.props;
+        const { accountsActive, accountsTerminated, user, hasZeroAccounts } = this.props;
+
+        const activeAccounts = accountsActive.map(account => (
+            <AccountPanel key={account.reference} account={account} user={user} />
+        ));
+        const terminatedAccounts = accountsTerminated.map(account => (
+            <AccountPanelTerminatedAccount key={account.reference} account={account} user={user} />
+        ));
+
         return (
             <AuthenticatedPageTemplate header={i18n('account.overview-header')}>
+                <Panel padding="20px" className="mb-8x">
+                    <ResponsivePanel desktop={3} tablet={2} mobile={1} horizontalGutter>
+                        <div>Lorem ipsum dolor sit amet</div>
+                        <div>Lorem ipsum dolor sit amet</div>
+                        <div>Lorem ipsum dolor sit amet</div>
+                        <div>Lorem ipsum dolor sit amet</div>
+                        <div>Lorem ipsum dolor sit amet</div>
+                        <div>Lorem ipsum dolor sit amet</div>
+                    </ResponsivePanel>
+                </Panel>
                 <div className="account-overview-page">
                     {hasZeroAccounts ? (
                         <NoAccountsPanel />
                     ) : (
-                        accountsActive.map(account => (
-                            <AccountPanel key={account.reference} account={account} user={user} />
-                        ))
+                        <>
+                            {activeAccounts}
+                            {terminatedAccounts}
+                        </>
                     )}
                     <FeedbackPanel />
                 </div>
