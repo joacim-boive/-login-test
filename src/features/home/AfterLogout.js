c16af6c55913d56970d5d2c136366b0a30d502df
Johan Linderoth <joli44@ecster.se>
2018-10-09 14:43:48 +0100

    After logout page, design and redirect

--- src/features/home/AfterLogout.js
+++ src/features/home/AfterLogout.js
@@ -7,33 +7,29 @@ import { Panel, ButtonGroup, LinkButton } from '@ecster/ecster-components';
 
 import PublicPageTemplate from '../common/templates/PublicPageTemplate';
 import { SURVEY_URL } from '../../common/surveyUrl';
+import icon from '../../common/images/icon-rocket.svg';
 
 export default class AfterLogout extends React.Component {
     render() {
+        const footer = (
+            <footer>
+                <a href="/">{i18n('home.after-logout.login-link')}</a>
+            </footer>
+        );
         return (
-            <PublicPageTemplate>
-                <div className="home-after-logout">
-                    <Panel withMixedContent centeredContent>
-                        <div className="mixed-content">
-                            <h2>{i18n('home.after-logout.header')}</h2>
-                            <p className="mb-8x">{i18n('home.after-logout.info')}</p>
-                            <ButtonGroup alignCenter spaceBelow={false}>
-                                <LinkButton
-                                    iconRight="icon-external-link"
-                                    href={SURVEY_URL}
-                                    target="_blank"
-                                    outline
-                                    round
-                                >
-                                    {i18n('home.feedback.link')}
-                                </LinkButton>
-                            </ButtonGroup>
-                        </div>
-                    </Panel>
-                    <footer>
-                        <a href="/">{i18n('home.after-logout.login-link')}</a>
-                    </footer>
-                </div>
+            <PublicPageTemplate footer={footer} className="home-after-logout">
+                <Panel withMixedContent centeredContent stretchInMobile>
+                    <div className="mixed-content">
+                        <img src={icon} className="mb-4x" />
+                        <h2>{i18n('home.after-logout.header')}</h2>
+                        <p className="info-text">{i18n('home.after-logout.info')}</p>
+                        <ButtonGroup alignCenter spaceBelow={false}>
+                            <LinkButton iconRight="icon-external-link" href={SURVEY_URL} target="_blank" outline round>
+                                {i18n('home.feedback.link')}
+                            </LinkButton>
+                        </ButtonGroup>
+                    </div>
+                </Panel>
             </PublicPageTemplate>
         );
     }
