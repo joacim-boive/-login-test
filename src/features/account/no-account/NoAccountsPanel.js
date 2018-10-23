eb3d880248406beccf06cecde2d78c7475648190
Johan Linderoth <joli44@ecster.se>
2018-10-23 14:39:38 +0100

    No accounts view with tmp images

--- src/features/account/no-account/NoAccountsPanel.js
+++ src/features/account/no-account/NoAccountsPanel.js
@@ -1,5 +1,13 @@
 import React, { Component } from 'react';
-import { Panel, ButtonGroup, LinkButton, UnorderedList, FlexPanel } from '@ecster/ecster-components';
+import {
+    Panel,
+    ButtonGroup,
+    LinkButton,
+    UnorderedList,
+    FlexPanel,
+    Mobile,
+    TabletOrDesktop,
+} from '@ecster/ecster-components';
 import { getText as i18n } from '@ecster/ecster-i18n/lib/Translate';
 import Lazyload from '../../../common/images/Lazyload';
 
@@ -14,25 +22,38 @@ export default class NoAccountsPanel extends Component {
         return (
             <div className="account-no-account">
                 <Panel withFullWidthContent>
-                    <img src={tmpImage} alt="" />
+                    <Mobile>
+                        <img
+                            src="https://res.cloudinary.com/johanlinderoth/image/upload/c_scale,w_768/v1540300071/girl-in-traffic-cropped.jpg"
+                            alt=""
+                        />
+                    </Mobile>
+                    <TabletOrDesktop>
+                        <img
+                            src="https://res.cloudinary.com/johanlinderoth/image/upload/c_scale,w_960/v1540300076/girl-in-traffic-wide.jpg"
+                            alt=""
+                        />
+                    </TabletOrDesktop>
 
                     <div className="two-col-content">
-                        <h2 className="e-green120 centered-content">{i18n('account.no-account.panel1.header')}</h2>
-                        <p>{i18n('account.no-account.panel1.text')}</p>
+                        <h2 className="h1 e-green centered-content">{i18n('account.no-account.panel1.header')}</h2>
                         <FlexPanel separator={false} reverseMobile>
-                            <UnorderedList icon="icon-check" iconClass="e-purple">
-                                {i18n('account.no-account.panel1.bullets', {
-                                    returnObjects: true,
-                                    wrapper: { tag: 'span', dangerouslySetInnerHTML: true },
-                                })}
-                            </UnorderedList>
+                            <div>
+                                <p>{i18n('account.no-account.panel1.text')}</p>
+                                <UnorderedList icon="icon-check" iconClass="e-purple">
+                                    {i18n('account.no-account.panel1.bullets', {
+                                        returnObjects: true,
+                                        wrapper: { tag: 'span', dangerouslySetInnerHTML: true },
+                                    })}
+                                </UnorderedList>
+                            </div>
                             <div className="ctr-2">
                                 <div className="info-bubble">Ingen årsavgift</div>
                                 <div className="ecster-card">
                                     <Lazyload
                                         className="lazyload"
                                         alt="Ecster Pay Card"
-                                        src={`/v1/cards/130.png`}
+                                        src="/v1/cards/130.png"
                                         widths={[166, 312]}
                                     />
                                 </div>
@@ -46,9 +67,20 @@ export default class NoAccountsPanel extends Component {
                     </ButtonGroup>
                 </Panel>
                 <Panel withFullWidthContent>
-                    <img src={tmpImage} alt="" />
+                    <Mobile>
+                        <img
+                            src="https://res.cloudinary.com/johanlinderoth/image/upload/c_scale,w_768/v1540301508/guitar-guy-cropped.jpg"
+                            alt=""
+                        />
+                    </Mobile>
+                    <TabletOrDesktop>
+                        <img
+                            src="https://res.cloudinary.com/johanlinderoth/image/upload/c_scale,w_960/v1540300080/guitar-guy.jpg"
+                            alt=""
+                        />
+                    </TabletOrDesktop>
                     <div className="mixed-content">
-                        <h2 className="e-green120 centered-content">{i18n('account.no-account.panel2.header')}</h2>
+                        <h2 className="h1 e-green centered-content">{i18n('account.no-account.panel2.header')}</h2>
                         <p>{i18n('account.no-account.panel2.text')}</p>
                     </div>
                     <ButtonGroup align="center">
