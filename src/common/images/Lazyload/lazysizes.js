5c34d8fce61817d07c2da98a82cc37c4e6fd2fd9
Joacim Boive <jobo15@handelsbanken.se>
2018-10-30 10:45:46 +0100

    Don't use the minified versions of lazysizes with react/webpack...

--- src/common/images/Lazyload/lazysizes.js
+++ src/common/images/Lazyload/lazysizes.js
@@ -4,7 +4,7 @@ import '../../util/handle-scaling';
 
 // Import the the main lazysizes library and needed plugins
 // NOTE! The plugin HAS to come before the main lazysizes library for it to work.
-import 'lazysizes/plugins/respimg/ls.respimg.min';
-import 'lazysizes/plugins/bgset/ls.bgset.min';
-import 'lazysizes/plugins/rias/ls.rias.min';
-import 'lazysizes/lazysizes.min';
+import 'lazysizes/plugins/respimg/ls.respimg';
+import 'lazysizes/plugins/bgset/ls.bgset';
+import 'lazysizes/plugins/rias/ls.rias';
+import 'lazysizes/lazysizes';
