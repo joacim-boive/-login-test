dccdb38cf259da63f2f7b7571bbb35bfd558d16a
Johan Linderoth <joli44@ecster.se>
2018-11-01 10:20:55 +0100

    Removed old GaRouteTracker

--- src/Root.js
+++ src/Root.js
@@ -6,7 +6,6 @@ import { Provider } from 'react-redux';
 import { Switch, HashRouter } from 'react-router-dom';
 import { GaProvider } from '@ecster/ecster-analytics/v2';
 import Authorized from './features/authentication/Authorized';
-// import GaRouteTracker from './features/common/ga/GaRouteTracker';
 
 import createRoutes from './createRoutes';
 
