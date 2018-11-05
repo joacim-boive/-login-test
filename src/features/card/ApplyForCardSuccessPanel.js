86e6323b0b7875ee948749fdd881c766418704b5
Johan Linderoth <joli44@ecster.se>
2018-11-05 13:59:02 +0000

    Apply success, same layout as failure + raise credit

--- src/features/card/ApplyForCardSuccessPanel.js
+++ src/features/card/ApplyForCardSuccessPanel.js
@@ -1,30 +1,27 @@
 import React from 'react';
-import PropTypes from 'prop-types';
+// import PropTypes from 'prop-types';
 
 import { getText as i18n } from '@ecster/ecster-i18n/lib/Translate';
-import { Panel } from '@ecster/ecster-components';
-import { EcsterCard } from '../common/card/EcsterCard';
+import { Panel, Link } from '@ecster/ecster-components';
+import happyFace from '../../common/images/face-happy.svg';
 
 export default class ApplyForCardSuccessPanel extends React.Component {
     static propTypes = {
-        account: PropTypes.shape().isRequired,
+        // account: PropTypes.shape().isRequired,
     };
 
     static defaultProps = {};
 
     render() {
-        const { account } = this.props;
-
         return (
-            <Panel withFullWidthContent sideMarginsInMobile className="card-apply-for-card-success-panel">
-                <div className="full-width-content flex-row">
-                    <div className="col1">
-                        <EcsterCard account={account} />
-                    </div>
-                    <div className="col2 centered-content">
-                        <h1 className="h3">{i18n('card.apply-for-card.success.header')}</h1>
-                        <p>{i18n('card.apply-for-card.success.info')}</p>
-                    </div>
+            <Panel withMixedContent centeredContent className="card-apply-for-card-success-panel">
+                <div className="mixed-content">
+                    <img src={happyFace} aria-hidden="true" alt="happy face icon" />
+                    <h1 className="h2">{i18n('card.apply-for-card.success.header')}</h1>
+                    <p>{i18n('card.apply-for-card.success.info')}</p>
+                    <Link iconLeft="icon-chevron-left" to="/account/overview">
+                        {i18n('card.apply-for-card.success.link')}
+                    </Link>
                 </div>
             </Panel>
         );
