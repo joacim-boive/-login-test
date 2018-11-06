20883880b70c9424b6689a4529ae86e44f22ca49
Johan Linderoth <joli44@ecster.se>
2018-11-06 09:55:02 +0000

    w.i.p

--- src/features/card/ApplyForCardPanel.js
+++ src/features/card/ApplyForCardPanel.js
@@ -12,12 +12,27 @@ export default class ApplyForCardPanel extends React.Component {
 
     static defaultProps = {};
 
+    state = {
+        checkboxChecked: false,
+    };
+
+    formRef = React.createRef();
+
+    checkboxRef = React.createRef();
+
     onClick = () => {
-        console.log('Applying for card... not yet implemented');
+        if (this.formRef.current.validate()) {
+            console.log('Applying for card... not yet implemented');
+        }
+    };
+
+    onCheckboxChange = ({ target }) => {
+        this.setState({ checkboxChecked: target.checked });
     };
 
     render() {
         const { account } = this.props;
+        const { checkboxChecked } = this.state;
 
         const cardName = (
             (account.product && account.product.name) ||
@@ -30,37 +45,46 @@ export default class ApplyForCardPanel extends React.Component {
                     <div className="two-col-content">
                         <EcsterCard account={account} />
                         <h1 className="h2">{i18n('card.apply-for-card.header', { cardName })}</h1>
-                        <FlexPanel>
-                            <div className="column-1">
-                                <p>{i18n('card.apply-for-card.info', { cardName: 'Ecster' })}</p>
-                                <p className="flex-row">
-                                    <span>{i18n('card.apply-for-card.yearly-fee')}</span>
-                                    <strong>15 000 kr</strong>
-                                </p>
-                                <p className="flex-row">
-                                    <span>{i18n('card.apply-for-card.credit-limit')}</span>
-                                    <strong>1 000 000 000 kr</strong>
-                                </p>
-                                <p dangerouslySetInnerHTML={{ __html: i18n('card.apply-for-card.card-terms') }} />
-                            </div>
-                            <div className="column-2">
-                                <UnorderedList icon="icon-check" iconClass="e-purple">
-                                    {i18n('card.apply-for-card.info-list', {
-                                        returnObjects: true,
-                                        wrapper: { tag: 'span', dangerouslySetInnerHTML: true },
-                                    })}
-                                </UnorderedList>
-                            </div>
-                        </FlexPanel>
-                        <ButtonGroup alignCenter spaceBelow={false}>
-                            <Button
-                                round
-                                onClick={this.onClick}
-                            >
-                                {i18n('card.apply-for-card.button')}
-                            </Button>
-                        </ButtonGroup>
+                        <Form ref={this.formRef} validateRefs={[this.checkboxRef]}>
+                            <FlexPanel>
+                                <div className="column-1">
+                                    <p>{i18n('card.apply-for-card.info', { cardName: 'Ecster' })}</p>
+                                    <p className="flex-row">
+                                        <span>{i18n('card.apply-for-card.yearly-fee')}</span>
+                                        <strong>15 000 kr</strong>
+                                    </p>
+                                    <p className="flex-row">
+                                        <span>{i18n('card.apply-for-card.credit-limit')}</span>
+                                        <strong>1 000 000 000 kr</strong>
+                                    </p>
+                                </div>
+                                <div className="column-2">
+                                    <UnorderedList icon="icon-check" iconClass="e-purple">
+                                        {i18n('card.apply-for-card.info-list', {
+                                            returnObjects: true,
+                                            wrapper: { tag: 'span', dangerouslySetInnerHTML: true },
+                                        })}
+                                    </UnorderedList>
+                                </div>
+                            </FlexPanel>
+                        </Form>
                     </div>
+                    <div className="centered-content">
+                        <Checkbox
+                            ref={this.checkboxRef}
+                            required
+                            checked={checkboxChecked}
+                            onChange={this.onCheckboxChange}
+                            validationMessage={i18n('card.apply-for-card.validation-message')}
+                        >
+                            <span dangerouslySetInnerHTML={{ __html: i18n('card.apply-for-card.card-terms') }} />
+                        </Checkbox>
+                    </div>
+                    <ButtonGroup alignColumn spaceBelow={false}>
+                        <Button round onClick={this.onClick}>
+                            {i18n('card.apply-for-card.button')}
+                        </Button>
+                    </ButtonGroup>
                 </Panel>
             </div>
         );
