79a6c7ce4b8f53cda8fdef874118c772920b5a16
Joacim Boive <jobo15@handelsbanken.se>
2018-11-14 09:50:01 +0100

    Fixes validation going blank for previous forms when submitting the entire page Fixes layout of final checkbox on the last step

--- tests/features/home/LoginHelp.test.js
+++ tests/features/home/LoginHelp.test.js
@@ -4,13 +4,9 @@ import { expect } from 'chai';
 import { LoginHelp } from 'src/features/home';
 
 describe('home/LoginHelp', () => {
-  it('renders node with correct class name', () => {
-    const renderedComponent = shallow(
-      <LoginHelp />
-    );
+    it('renders node with correct class name', () => {
+        const renderedComponent = shallow(<LoginHelp />);
 
-    expect(
-      renderedComponent.find('.home-login-help').getElement()
-    ).to.exist;
-  });
+        expect(renderedComponent.find('.home-login-help').getElement()).to.exist;
+    });
 });
