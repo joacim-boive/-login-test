04b35a1722c1c09e584aef965c0660841ba13742
Joacim Boive <jobo15@handelsbanken.se>
2018-12-28 14:02:40 +0100

    Video background for desktop

--- src/features/common/templates/LoginPageTemplate.js
+++ src/features/common/templates/LoginPageTemplate.js
@@ -1,9 +1,44 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import { Logo, Link } from '@ecster/ecster-components';
+import VideoCover from 'react-video-cover';
+
+import detectDevice from '@ecster/ecster-components/util/detect-device';
+
 import cloudinary from '../../../common/images/Lazyload/cloudinary';
 
 import MessagePanel from '../MessagePanel';
+
+const Wrapper = ({ children }) => {
+    const videoOptions = {
+        src: 'https://res.cloudinary.com/ecster/video/upload/v1/backgrounds/kustlinje.mp4',
+        remeasureOnWindowResize: true,
+        autoPlay: true,
+        muted: true,
+        loop: true,
+    };
+
+    return detectDevice().isDesktop ? (
+        <article className="common-login-page-template is-desktop">
+            <VideoCover videoOptions={videoOptions} />
+            {children}
+        </article>
+    ) : (
+        <article
+            className="common-login-page-template lazyload"
+            data-bgset={`${cloudinary.defaults},o_40${cloudinary.scaling}/v1/backgrounds/login.jpg`}
+            data-absurl="false"
+            data-sizes="auto"
+            data-widths="768, 1536, 1024, 2048, 1920, 3840"
+        >
+            {children}
+        </article>
+    );
+};
+
+Wrapper.propTypes = {
+    children: PropTypes.node.isRequired,
+};
 
 const LoginPageTemplate = props => {
     const { children } = props;
@@ -16,20 +51,15 @@ const LoginPageTemplate = props => {
     window.lazySizesConfig.rias.widths = [768, 1536, 1024, 2048, 1920, 3840];
     return (
         <>
-            <article
-                className="common-login-page-template lazyload"
-                data-bgset={`${cloudinary.defaults},o_40${cloudinary.scaling}/v1/backgrounds/login.jpg`}
-                data-absurl="false"
-                data-sizes="auto"
-                data-widths="768, 1536, 1024, 2048, 1920, 3840"
-            >
+            <Wrapper>
                 <header className="logo-header">
                     <Link href="https://www.ecster.se" target="_blank" title="ecster.se" id="login-page-logo-link">
                         <Logo withName isNoWrap />
                     </Link>
                 </header>
                 {children}
-            </article>
+            </Wrapper>
+
             <MessagePanel />
         </>
     );
